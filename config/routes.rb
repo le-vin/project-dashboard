7513dfd93839ef565c3e31d9fa002a13786254d5
Levin Bauschke <b.bauschke@zeppelin-university.net>
2022-06-30 10:12:55 +0200

    Add Dashboard scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :dashboards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'dashboards#index'
+  resources :dashboards, except: :index
   # Defines the root path route ("/")
   # root "articles#index"
 end
---
+++ db/migrate/20220630081114_create_dashboards.rb
@@ -0,0 +1,10 @@
+class CreateDashboards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dashboards do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
